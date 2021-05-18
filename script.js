d1a12f31e202cdb7b65accd815508e59216b1e77
ramironinni <ramironinni@gmail.com>
2021-05-17 21:10:45 -0300

    Update squares to size to container

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.addEventListener("DOMContentLoaded", createEtchASketch);
 
 function createEtchASketch() {
-    createSquaresContainer();
+    const squaresContainer = createSquaresContainer();
+    createSquares(squaresContainer, 4);
     createCustomizeBtn();
 }
 
@@ -11,23 +12,20 @@ function createSquaresContainer() {
     const body = document.body;
     squaresContainer.addEventListener("mouseover", colorizeSquare);
     body.appendChild(squaresContainer);
-    drawSquares(squaresContainer);
+    return squaresContainer;
 }
 
-function createCustomizeBtn() {
-    const customizeBtn = document.createElement("button");
-    customizeBtn.classList.add("customize-button");
-    customizeBtn.innerText = "Customize!";
-    document.body.appendChild(customizeBtn);
-    customizeBtn.addEventListener("click", askNumberSquares);
-}
+function createSquares(container, squares) {
+    const containerWidth = window.getComputedStyle(container).width;
+    const containerWithNum = containerWidth.slice(0, length - 2);
+    const totalSquares = squares * squares;
+    const squareSize = containerWithNum / squares;
 
-function drawSquares(squaresContainer, squares = 4) {
-    const totalSquares = squares * 4;
     for (let i = 0; i < totalSquares; i++) {
-        const plainSquare = document.createElement("div");
-        plainSquare.classList.add("square");
-        squaresContainer.appendChild(plainSquare);
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${squareSize}px`;
+        container.appendChild(square);
     }
 }
 
@@ -36,6 +34,14 @@ function colorizeSquare(e) {
         randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
         e.target.style.backgroundColor = randomColor;
     }
+}
+
+function createCustomizeBtn() {
+    const customizeBtn = document.createElement("button");
+    customizeBtn.classList.add("customize-button");
+    customizeBtn.innerText = "Customize!";
+    document.body.appendChild(customizeBtn);
+    customizeBtn.addEventListener("click", askNumberSquares);
 }
 
 function askNumberSquares() {
